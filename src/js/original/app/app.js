3901aa925360f47c30a54e9f1eef76f8a68b7ed0
ArmanFanavaran <103922832+ArmanFanavaran@users.noreply.github.com>
2025-06-08 19:38:32 +0330

    Update app.js

--- src/js/original/app/app.js
+++ src/js/original/app/app.js
@@ -313,7 +313,7 @@ audioCat.app.App = function(domHelper, project) {
   // Manages recording.
   var mediaSourceObtainer = new audioCat.audio.record.MediaSourceObtainer(
       idGenerator,
-      supportDetector
+      supportDetector,
       audioContextManager);
   var recordManager = new audioCat.audio.record.MediaRecordManager(
       audioContextManager,
